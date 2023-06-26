53a92ec2619249afda6c721e5dc4986a83ebfedf
Prakshal Jain <prakshalj0512@gmail.com>
2023-06-26 13:32:43 -0700

    pr_generator

---
+++ notebooks/pr_generator.py
@@ -0,0 +1,41 @@
+import git
+from github import Github
+
+# GitHub access token
+# Load the GitHub access token from the YAML file
+with open("config.yaml", "r") as file:
+    config = yaml.safe_load(file)
+access_token = config["github"]["access_token"]
+
+# Initialize the Git repository
+repo = git.Repo()
+
+# Get the current branch
+current_branch = repo.active_branch
+
+# Get the diff between the current branch and main
+diff = repo.git.diff('main', current_branch)
+
+# Print the diff
+print(diff)
+
+# Create a new GitHub instance
+github = Github(access_token)
+
+# Get the repository
+repo_name = "prakshalj0512/databricks-llm"
+repository = github.get_repo(repo_name)
+
+# Create a pull request
+pr_title = "Pull Request Title"
+pr_body = "Pull Request Body"
+
+# Specify the base branch and head branch for the pull request
+base_branch = "main"
+head_branch = current_branch.name
+
+# Create the pull request
+pull_request = repository.create_pull(title=pr_title, body=pr_body, base=base_branch, head=head_branch)
+
+# Print the pull request number
+print("Pull Request Created - Number:", pull_request.number)
